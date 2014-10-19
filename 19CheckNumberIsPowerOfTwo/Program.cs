d0a39798b61b01475e0c016350b7a99d71808fb9
ArtyTheTorch <artysaligator@yahoo.com>
2014-10-19 12:05:31 -0700

    Checks a number x, and returns T/F if Log_2(x)=int
    Converts number to binary. If a number is a leading 1 by all 0's then it
    is power of two.
    Console-Implementation

---
+++ 19CheckNumberIsPowerOfTwo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _19CheckNumberIsPowerOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(convertToBinary(num));
+            Console.WriteLine(isPowerOfTwo(num));
+            //Console.WriteLine(isPowerOfTwo(num));   
+            Console.ReadLine();
+        }
+
+        private static bool isPowerOfTwo(int num)
+        {
+            bool results = true;
+            double temp = convertToBinary(num);
+            while (temp > 1)
+            {
+                if (temp % 10 > 0)
+                {
+                    results = false;
+                    break;
+                }
+                temp = temp - temp % 10;
+                temp = temp / 10;
+            }
+            
+            return results;
+        }
+
+        private static double convertToBinary(int num)
+        {
+            int results = 0;
+            Stack<int> digits = new Stack<int>();
+            int temp = num;
+            int f = 0;
+
+            while (temp > 0) {
+                digits.Push(temp % 2);
+                temp = temp / 2;
+            }
+
+            //put the results into a string
+            while (digits.Count !=0)
+            {
+                f = digits.Count - 1;
+                results += digits.Pop() * Convert.ToInt32(Math.Pow(10,f));
+            }
+            return results;
+        }
+    }
+}
